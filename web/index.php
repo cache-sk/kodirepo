406a6c5a1c9a973165f16485b588083d8f12e26b
cache-sk <cache.sk@gmail.com>
2021-02-26 13:54:59 +0100

    web update

--- web/index.php
+++ web/index.php
@@ -1,14 +1,28 @@
 <?
+    $repo_prefix = 'repository.cache-sk-';
 	if (isset($_GET["path"])){
-		header("Location: https://github.com/cache-sk/kodirepo/raw/master/repository/repository.cache-sk/".$_GET["path"]);
+        if (false !== strpos($_GET["path"],$repo_prefix)){
+		    header("Location: https://github.com/cache-sk/kodirepo/raw/master/repository/repository.cache-sk/".$_GET["path"]);
+        }
 		die;
 	}
 	$contents = file_get_contents("https://github.com/cache-sk/kodirepo/tree/master/repository/repository.cache-sk");
 	preg_match_all('/.*title="repository.cache-sk-([0-9]+.[0-9]+.[0-9]+).zip".*/', $contents, $matches);
+    
+    $ignore_files = ['.htaccess','index.php'];
+    $files = array_filter(scandir('.'), function($item){
+        global $ignore_files;
+        return is_file($item) && !in_array($item,$ignore_files);
+    });
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <html>
 <body>
-<a href="repository.cache-sk-<?=$matches[1][0]?>.zip">repository.cache-sk-<?=$matches[1][0]?>.zip</a>
+<a href="repository.cache-sk-<?=$matches[1][0]?>.zip">repository.cache-sk-<?=$matches[1][0]?>.zip</a><br/>
+<?
+    foreach ($files as $fname){
+        echo("<a href=\"$fname\">$fname</a><br/>\n");
+    }
+?>
 </body>
 </html>
