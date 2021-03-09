c990735094a68b3c30767c288f02747ceaf0d7a1
cache-sk <cache.sk@gmail.com>
2021-03-09 12:41:50 +0100

    regenerate 20210309.1

--- web/index.php
+++ web/index.php
@@ -1,18 +1,22 @@
 <?
     $repo_prefix = 'repository.cache-sk-';
+    $files_prefix = './files/';
+    $ignore_files = array('.htaccess','.gitkeep','index.php');
+
 	if (isset($_GET["path"])){
         if (false !== strpos($_GET["path"],$repo_prefix)){
 		    header("Location: https://github.com/cache-sk/kodirepo/raw/master/repository/repository.cache-sk/".$_GET["path"]);
+        } else {
+            header("Location: ".$files_prefix.$_GET["path"]);
         }
 		die;
 	}
 	$contents = file_get_contents("https://github.com/cache-sk/kodirepo/tree/master/repository/repository.cache-sk");
 	preg_match_all('/.*title="repository.cache-sk-([0-9]+.[0-9]+.[0-9]+).zip".*/', $contents, $matches);
     
-    $ignore_files = ['.htaccess','index.php'];
-    $files = array_filter(scandir('.'), function($item){
-        global $ignore_files;
-        return is_file($item) && !in_array($item,$ignore_files);
+    $files = array_filter(scandir($files_prefix), function($item){
+        global $files_prefix, $ignore_files;
+        return is_file($files_prefix.$item) && !in_array($item,$ignore_files);
     });
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
